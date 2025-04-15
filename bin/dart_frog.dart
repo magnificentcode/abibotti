32e09a1ee4da9f321c849929a753db2afa8b3e31
magnificentcode <140209973+magnificentcode@users.noreply.github.com>
2025-04-15 16:38:37 +0000

    fix: remove buildHandler and use handler with serve()

--- bin/dart_frog.dart
+++ bin/dart_frog.dart
@@ -4,6 +4,5 @@ import 'package:dart_frog/dart_frog.dart';
 
 void main() async {
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final handler = await buildHandler();
   await serve(handler, InternetAddress.anyIPv4, port);
 }
