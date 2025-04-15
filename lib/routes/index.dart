551b792d99f8ed3190555235074d9a947b651c6a
magnificentcode <140209973+magnificentcode@users.noreply.github.com>
2025-04-15 19:17:50 +0000

    feat: serve index.html at /

--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -3,7 +3,7 @@ import 'package:dart_frog/dart_frog.dart';
 Future<Response> onRequest(RequestContext context) async {
   // Servir le fichier public/main.html
   return Response.file(
-    'public/main.html',
+    'public/index.html',
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
