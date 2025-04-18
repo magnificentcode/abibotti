65d6fb378c1b0d1479b516a6e0fe233f93c1c0a9
magnificentcode <140209973+magnificentcode@users.noreply.github.com>
2025-04-18 10:42:55 +0000

    fix: use absolute path to serve main.html

--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
 
 Future<Response> onRequest(RequestContext context) async {
+  final path = Directory.current.path + '/public/main.html';
+  final file = File(path);
+
+  if (!file.existsSync()) {
+    return Response(statusCode: 404, body: 'Fichier main.html introuvable');
+  }
+
   return Response.file(
-    'public/main.html',
-    headers: {
-      'Content-Type': 'text/html; charset=utf-8',
-    },
+    file.path,
+    headers: {'Content-Type': 'text/html; charset=utf-8'},
   );
 }
